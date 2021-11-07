54e428261d2ba332981bfdf2f3d24da3ef83d1d1
Arian <arian.kulmer@web.de>
2021-11-07 19:17:23 +0100

    redwood: Set bootloader board name
    Change-Id: Ied8de2941ccc455c7755f50e3843eadde59f1ad8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/redwood
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
+# Board
+TARGET_BOOTLOADER_BOARD_NAME := redwood
+
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/redwood_QGKI.config
 
